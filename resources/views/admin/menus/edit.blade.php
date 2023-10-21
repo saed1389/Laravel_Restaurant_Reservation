545090afb7d7fa16770cec96e931f450edf254c2
saed1389 <saed.1389@gmail.com>
2023-10-21 14:59:20 +0300

    Edit Delete Menus

---
+++ resources/views/admin/menus/edit.blade.php
@@ -0,0 +1,68 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.menus.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white"> Menu Index</a>
+            </div>
+            <div class="m-2 p-2 bg-white rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form method="post" action="{{ route('admin.menus.update', $menu->id) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="sm:col-span-6">
+                            <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
+                            <div class="mt-1">
+                                <input type="text" id="name" name="name" value="{{ $menu->name }}" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <div>
+                                <img src="{{ Storage::url($menu->image) }}" class="w-32 h-32" alt="">
+                            </div>
+                            <div class="mt-1">
+                                <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="price" class="block text-sm font-medium text-gray-700"> Price </label>
+                            <div class="mt-1">
+                                <input type="number" id="price" name="price" min="0.00" max="10000.00" value="{{ $menu->price }}" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                            <div class="mt-1">
+                                <textarea id="description" rows="3" name="description"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm ">
+                                    {{ $menu->description }}
+                                </textarea>
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="categories" class="block text-sm font-medium text-gray-700">Categories</label>
+                            <div class="mt-1">
+                                <div class="mt-1">
+                                    <select id="categories" name="categories[]" class="rounded form-multiselect block w-full mt-1 border border-gray-400" multiple>
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}" @selected($menu->categories->contains($category))> {{ $category->name }} </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-6 p-4">
+                            <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-admin-layout>
