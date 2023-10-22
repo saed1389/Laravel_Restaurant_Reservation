a63a4ff54e61f7d65fcf2190557e2590e91aace8
saed1389 <saed.1389@gmail.com>
2023-10-22 16:00:00 +0300

    Table, Date, Guests

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -73,7 +73,7 @@
                 @endif
                 @if(session()->has('warning'))
                         <div class="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800" role="alert">
-                            <span class="font-medium">{{ session()->has('warning') }}!</span>
+                            <span class="font-medium">{{ session()->get('warning') }}!</span>
                         </div>
                 @endif
 
